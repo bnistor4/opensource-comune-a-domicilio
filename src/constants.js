6cc4f35b92481cdb5f2e1a56c19960d83e1903e1
Matteo Errera <info@r2a.me>
2020-04-22 23:26:36 +0200

    fixed constants

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const ID_GOOGLE_SPREADSHEET = "1ORBU2WqDBQvwZsNO2U9QobzbyvgQ48odu1knZ4O2Wkg";
-export const ADD_ACTIVITY_URL = "https://bit.ly/fiumicinodomicilio";
-export const PRIVACY_POLICY_URL = "https://www.iubenda.com/privacy-policy/14777174";
+export const ADD_ACTIVITY_URL = "https://typeform.com";
+export const PRIVACY_POLICY_URL = "#";
 export const VERSION = "v0.0.1";
-export const APP_NAME = "Fiumicino a Domicilio";
+export const APP_NAME = "Comune a Domicilio";
