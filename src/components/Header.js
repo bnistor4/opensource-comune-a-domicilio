666586e68245f1128f86ad64d529a4e5162639de
bnistor4 <bnistor4@gmail.com>
2020-04-26 17:45:21 +0200

    Header update
    Message update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,19 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.svg";
 import React from "react";
-import {APP_NAME} from "../constants";
+import { APP_NAME } from "../constants";
 
-export const Header = ({children}) => {
+export const Header = ({ children }) => {
     return (
         <header className="App-header">
             <div className="container">
-                    <Link className={'main-logo'} to="/">{APP_NAME}</Link>
-                    <p>Questo sito ha l'utilità di dare informazioni relative all'apertura ed a servizi offerti, fra cui la consegna a domicilio.</p>
-                    <p>
-                        Registrazione gratuita per sempre.<br/>
+                <Link className={'main-logo'} to="/">{APP_NAME}</Link>
+                <p>Questo sito ha lo scopo di dare informazioni relative all'apertura ed a servizi offerti dai commercianti di zona, fra cui la <stong>consegna a domicilio.</stong></p>
+                <p>
+                    Registrazione gratuita per sempre.<br />
                         Aiutiamoci in questo momento di difficoltà 😄
                     </p>
-                    {children}
+                {children}
             </div>
         </header>
     )
